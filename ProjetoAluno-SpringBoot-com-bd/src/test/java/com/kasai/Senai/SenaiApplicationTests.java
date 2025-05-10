321a1e602267de82f888a834e582237abfd66ac8
Taina <tainakasai01@gmail.com>
2025-05-10 09:54:15 -0300

    Create new project

---
+++ ProjetoAluno-SpringBoot-com-bd/src/test/java/com/kasai/Senai/SenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasai.Senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
