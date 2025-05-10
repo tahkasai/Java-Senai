ff8188bd70024b3ef4b80c77fcdf3c89939a0b00
Taina <tainakasai01@gmail.com>
2025-05-10 10:42:07 -0300

    update alunoService

--- ProjetoAluno-SpringBoot-com-bd/src/main/java/com/kasai/Senai/Controlle/AlunoControlle.java
+++ ProjetoAluno-SpringBoot-com-bd/src/main/java/com/kasai/Senai/Controlle/AlunoControlle.java
@@ -1,4 +1,32 @@
 package com.kasai.Senai.Controlle;
 
 public class AlunoControlle {
+    private final AlunoService alunoService;
+
+    public AlunoControlle(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<Aluno> listaDeAlunos() {
+        return alunoService.buscarAlunos();
+    }
+
+    @PostMapping
+    public Aluno criar(@RequestBody Aluno aluno) {
+        return alunoService.salvarAluno(aluno);
+    }
+
+    @GetMapping("/id")
+    public Aluno BuscarAlunoId(Long Id){
+        return alunoService.buscarAlunoId(Id);
+    }
+
+    @GetMapping("/id")
+    public void deletarAluno(@PathVariable Long Id){
+        alunoService.deletarAluno(Id);
+    }
+
+
+
 }
