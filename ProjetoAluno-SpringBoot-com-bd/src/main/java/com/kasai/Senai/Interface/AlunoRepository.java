37f3539663b0d003623bb76064e6d4bed0dd9017
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-05-10 15:47:24 -0300

    Delete ProjetoAluno-SpringBoot-com-bd directory

--- ProjetoAluno-SpringBoot-com-bd/src/main/java/com/kasai/Senai/Interface/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kasai.Senai.Interface;
-
-import com.kasai.Senai.Model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno,Long> {
-
-}
